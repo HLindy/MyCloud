fc2da19f38c1a11f95dce4a6db8f7a5989ce7332
houge <740825930@qq.com>
2017-03-15 22:34:34 -0700

    initial commit

--- MyCloud/settings.py
+++ MyCloud/settings.py
@@ -11,12 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-
-print("hello")
-print("houge")
-
-print("world")
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
